79902c929a66a030f2a00691c314d2f8d3a00060
Rashel Rojas <rdr2139@columbia.edu>
2023-03-21 11:20:13 -0400

    basic website, restarted

--- dynamicextension/scripts/reader.js
+++ dynamicextension/scripts/reader.js
@@ -1,24 +1,44 @@
-var currentURL = window.location.href;
-
-console.log(currentURL);
-
-var htmlCode;
-// Stack Overflow: https://stackoverflow.com/questions/6375461/get-html-code-using-javascript-with-a-url
-// Stack Overflow: https://stackoverflow.com/questions/2014570/save-data-from-ajax-request-in-variable-jquery
-$.ajax({
-    url: currentURL,
-    success: function(data) {
-        useReturnData(data)
-    }
-});
-
-function useReturnData(data){
-    htmlCode = data;
-    console.log(htmlCode);
-};
-
-// Level 1: Use accessible methods to implement error messages
-// Inject error messages in the correct reading order in the DOM.
-// If a suggestion for fixing an error is provided, associate it with the input element using aria-describedby.
-// Set the aria-invalid attribute to “true” on elements with an input error (and remove it once the error is fixed).
-// Use alert role or aria-live regions to surface error messages that do not take focus.
+$(document).ready(function() {
+    // Play video
+    $("#videoplayer").mousedown(function() {
+  
+      if($($("#videoplayer").children()[0]).attr("src")=="pics/play.png") {
+        $($("#videoplayer").children()[0]).attr("src","pics/pause.jpg");
+        $("#thevideo")[0].play();
+      } else {
+        $($("#videoplayer").children()[0]).attr("src","pics/play.png");
+        $("#thevideo")[0].pause();
+      }
+    });
+    
+  
+    // Form validation
+    $("#signupbutton").click(function() {
+      if($("#fn").val()=="") {
+        $("#firstName").addClass("error");
+  
+        return;
+      } else {
+        $("#firstName").removeClass("error");
+      }
+  
+      if($("#ln").val()=="") {
+        $("#lastName").addClass("error");
+  
+        return;
+      } else {
+        $("#lastName").removeClass("error");
+      }
+  
+      if($("#em").val()=="") {
+        $("#email").addClass("error");
+  
+        return;
+      } else {
+        $("#email").removeClass("error");
+      }
+  
+      alert("Thank you!  Please watch your email for our exciting newsletter and offers!");
+    });
+  });
+  
