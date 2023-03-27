abf700b070fe9b4b0a59d66b105ba88efedac660
Rashel Rojas <rdr2139@columbia.edu>
2023-03-26 23:19:34 -0400

    Added hover content section

--- dynamicextension/scripts/reader.js
+++ dynamicextension/scripts/reader.js
@@ -28,46 +28,60 @@ $(document).ready(function() {
   });
 
 
-    // Play video
-    $("#videoplayer").mousedown(function() {
-  
-      if($($("#videoplayer").children()[0]).attr("src")=="pics/play.png") {
-        $($("#videoplayer").children()[0]).attr("src","pics/pause.jpg");
-        $("#thevideo")[0].play();
-      } else {
-        $($("#videoplayer").children()[0]).attr("src","pics/play.png");
-        $("#thevideo")[0].pause();
-      }
-    });
+  // Play video
+  $("#videoplayer").mousedown(function() {
+
+    if($($("#videoplayer").children()[0]).attr("src")=="pics/play.png") {
+      $($("#videoplayer").children()[0]).attr("src","pics/pause.jpg");
+      $("#thevideo")[0].play();
+    } else {
+      $($("#videoplayer").children()[0]).attr("src","pics/play.png");
+      $("#thevideo")[0].pause();
+    }
+  });
     
   
-    // Form validation
-    $("#signupbutton").click(function() {
-      if($("#fn").val()=="") {
-        $("#firstName").addClass("error");
-  
-        return;
-      } else {
-        $("#firstName").removeClass("error");
-      }
-  
-      if($("#ln").val()=="") {
-        $("#lastName").addClass("error");
-  
-        return;
-      } else {
-        $("#lastName").removeClass("error");
-      }
-  
-      if($("#em").val()=="") {
-        $("#email").addClass("error");
-  
-        return;
-      } else {
-        $("#email").removeClass("error");
-      }
-  
-      alert("Thank you!  Please watch your email for our exciting newsletter and offers!");
-    });
+  // Form validation
+  $("#signupbutton").click(function() {
+    if($("#fn").val()=="") {
+      $("#firstName").addClass("error");
+
+      return;
+    } else {
+      $("#firstName").removeClass("error");
+    }
+
+    if($("#ln").val()=="") {
+      $("#lastName").addClass("error");
+
+      return;
+    } else {
+      $("#lastName").removeClass("error");
+    }
+
+    if($("#em").val()=="") {
+      $("#email").addClass("error");
+
+      return;
+    } else {
+      $("#email").removeClass("error");
+    }
+
+    alert("Thank you!  Please watch your email for our exciting newsletter and offers!");
   });
+
+  // Good Hover Content Example
+  var parent = document.getElementById('parent');
+
+  parent.onmouseover = function() {
+      document.getElementById('popup').style.display = 'block';
+  }
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code == 'Escape')
+        document.getElementById('popup').style.display = 'none';
+  });
+
+
+});
   
